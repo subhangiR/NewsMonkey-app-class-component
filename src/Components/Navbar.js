d54a6607d25092c293f26ab626bce72cbbd8c300
Subhangi Raj <subhangiraj@nkw.pub>
2025-05-01 19:29:56 +0530

    Class component App

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';  // Import Link from react-router-dom
+import '../App.css';
+
 
 export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand text-white" to="/">Navbar</Link>
+          <Link className="navbar-brand text-white" to="/">News</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
